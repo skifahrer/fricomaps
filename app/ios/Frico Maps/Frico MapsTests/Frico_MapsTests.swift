31953901876da8afb82300027cf19a4724ddc2e4
skifahrer <r.dvorsky@klarstein.com>
2026-08-10 09:30:57 +0200

    mobile app

---
+++ app/ios/Frico Maps/Frico MapsTests/Frico_MapsTests.swift
@@ -0,0 +1,18 @@
+//
+//  Frico_MapsTests.swift
+//  Frico MapsTests
+//
+//  Created by mac on 02.08.2026.
+//
+
+import Testing
+
+struct Frico_MapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
